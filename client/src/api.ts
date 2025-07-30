b4f12876f5a13a7681bf0c8bcf72831c1bb76a4b
Udit <145645636+uditttttt@users.noreply.github.com>
2025-07-30 21:30:38 +0530

    Update api.ts

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://note-taking-app-6jq8.onrender.com',
 });
 
 // Interceptor to add the JWT token to every request
